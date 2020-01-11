0e9c8c883d0d67ba17754c719252172ab2ff48c0
Bruno Zanin <corporate.zanin@gmail.com>
2020-01-11 15:48:48 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,15 +12,21 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
+		  @post = Post.new(params.require(:post).permit(:title, :description))
+  @post.save
+  redirect_to post_path(@post)
+	#   @post = Post.new(params["post"])
+	#   @post.save
+	#   redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
+		@post = Post.find(params[:id])
+  @post.update(params.require(:post).permit(:title))
+  redirect_to post_path(@post)
+	#   @post = Post.find(params[:id])
+	#   @post.update(params["post"])
+	#   redirect_to post_path(@post)
 	end
 
 	def edit
